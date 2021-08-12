bd2805b960aafd91567a7183d581b3ce59e1c492
tituspijean <tituspijean@outlook.com>
2021-08-12 17:09:43 +0200

    Try to fix install and postinstall

---
+++ conf/config.php
@@ -0,0 +1,7 @@
+<?php
+            define('DB_HOST', 'localhost');
+            define('DB_PORT', '3306');
+            define('DB_NAME', '__DB_NAME__');
+            define('DB_USER', '__DB_NAME__');
+            define('DB_PASSWORD', '__DB_PWD__');
+            define('SECRET_KEY', '__SECRET_KEY__');
