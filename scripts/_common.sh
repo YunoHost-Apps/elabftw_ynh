e8fba0d52b5d4bb5b4177d135fd6617dde3fe8b1
ericgaspar <junk.eg@free.fr>
2022-05-17 14:03:50 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -50,33 +50,3 @@ YNH_COMPOSER_VERSION="2.2.7"
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
-
-
-    php${YNH_PHP_VERSION}-bcmath \
-    php${YNH_PHP_VERSION}-curl \
-    php${YNH_PHP_VERSION}-ctype \
-    php${YNH_PHP_VERSION}-dev \
-    php${YNH_PHP_VERSION}-dom \
-    php${YNH_PHP_VERSION}-exif \
-    php${YNH_PHP_VERSION}-gd \
-    php${YNH_PHP_VERSION}-gettext \
-    php${YNH_PHP_VERSION}-fileinfo \
-    php${YNH_PHP_VERSION}-fpm \
-    php${YNH_PHP_VERSION}-json \
-    php${YNH_PHP_VERSION}-intl \
-    php${YNH_PHP_VERSION}-ldap \
-    php${YNH_PHP_VERSION}-mbstring \
-    php${YNH_PHP_VERSION}-opcache \
-    php${YNH_PHP_VERSION}-openssl \
-    php${YNH_PHP_VERSION}-pdo_mysql \
-    php${YNH_PHP_VERSION}-pecl-imagick \
-    php${YNH_PHP_VERSION}-phar \
-    php${YNH_PHP_VERSION}-redis \
-    php${YNH_PHP_VERSION}-simplexml \
-    php${YNH_PHP_VERSION}-session \
-    php${YNH_PHP_VERSION}-sodium \
-    php${YNH_PHP_VERSION}-tokenizer \
-    php${YNH_PHP_VERSION}-xml \
-    php${YNH_PHP_VERSION}-xmlwriter \
-    php${YNH_PHP_VERSION}-zip \
-    php${YNH_PHP_VERSION}-zlib
