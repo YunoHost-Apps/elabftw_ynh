96813f35539d8ec1f7b16f77f5b8a4d9b2fa04e3
ericgaspar <junk.eg@free.fr>
2022-05-17 12:09:57 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,12 +5,12 @@
 # COMMON VARIABLES
 #=================================================
 
-nodejs_version=16
+nodejs_version=18
 
 YNH_PHP_VERSION="8.0"
 
 # dependencies used by the app
-pkg_dependencies="brotli php${YNH_PHP_VERSION}-pdo_mysql php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-gettext php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-xml"
+pkg_dependencies="brotli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-gettext php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-xml"
 
 YNH_COMPOSER_VERSION="2.3.5"
 
