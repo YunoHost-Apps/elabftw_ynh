f6a5754ee16ab0062284bc2f96b6f7090b9cacb3
ericgaspar <junk.eg@free.fr>
2022-05-17 11:38:50 +0200

    Fix

--- conf/config.php
+++ conf/config.php
@@ -2,6 +2,6 @@
             define('DB_HOST', 'localhost');
             define('DB_PORT', '3306');
             define('DB_NAME', '__DB_NAME__');
-            define('DB_USER', '__DB_NAME__');
+            define('DB_USER', '__DB_USER__');
             define('DB_PASSWORD', '__DB_PWD__');
             define('SECRET_KEY', '__SECRET_KEY__');
