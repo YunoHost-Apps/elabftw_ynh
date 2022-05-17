848361ce52813c1f08694937771765dcd33fb2d2
ericgaspar <junk.eg@free.fr>
2022-05-17 12:34:04 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,7 +12,7 @@ YNH_PHP_VERSION="8.0"
 # dependencies used by the app
 pkg_dependencies="zopfli brotli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-gettext php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-xml"
 
-YNH_COMPOSER_VERSION="2.3.5"
+YNH_COMPOSER_VERSION="2.2.7"
 
 #=================================================
 # PERSONAL HELPERS
