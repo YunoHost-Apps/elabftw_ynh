b730c3160f8c45119d15e76c699cab374426924b
yalh76 <yalh@yahoo.com>
2025-01-26 15:08:30 +0100

    Upgrade to 4.5.14

--- conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-            define('DB_HOST', 'localhost');
-            define('DB_PORT', '3306');
-            define('DB_NAME', '__DB_NAME__');
-            define('DB_USER', '__DB_USER__');
-            define('DB_PASSWORD', '__DB_PWD__');
-            define('SECRET_KEY', '__SECRET_KEY__');
-            define('SITE_URL', 'https://__DOMAIN__');
